77df328843a1dbd9b5049ca18209e17154890b93
Kyant0 <kyant2021@outlook.com>
2022-08-30 09:52:00 +0800

    Update JavaVersion to 11

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -23,15 +23,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
         freeCompilerArgs = freeCompilerArgs + listOf("-Xexplicit-api=strict")
     }
     buildFeatures {
